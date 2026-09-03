3f7f9103e129a79fb36d5556221c33925e6efd8b
aautarch <k.nitta.author@gmail.com>
2026-09-03 10:18:27 +0800

    📍:unit: unit related classes set
    - map_unit modified
    - unit.cs base struct modified with methods
    - test characters.json created

--- src/Unit.cs
+++ src/Unit.cs
@@ -1,8 +1,8 @@
-using System;
+using Godot;
+using Godot.Collections;
 
 public struct Unit
 {
-
     public enum CHR_CLASS: byte
     {
         NULL,
@@ -29,4 +29,34 @@ public struct Unit
         this.name = name;
         this.char_class = char_class;
     }
+
+    public static string getClassNameByIdx(byte idx)
+    {
+        string[] class_names = {"null", "exorcist", "paladin", "citizen", "vanguard", "journeyman", "courier", "scryer", "hunter"};
+
+        return class_names[idx];
+    }
+
+    public static Unit[] loadCharacters()
+    {
+        string fileName = "res://data/characters.json";
+
+        var f = FileAccess.GetFileAsString(fileName);
+
+        Dictionary j = (Dictionary) Json.ParseString(f);
+
+        Array characters = (Array) j["characters"];
+
+        Unit[] units = new Unit[characters.Count];
+
+        for (byte i = 0; i < characters.Count; i++)
+        {
+            Dictionary item = (Dictionary) characters[i];
+
+            units[i] = new Unit(0, (string) item["name"], (Unit.CHR_CLASS) (byte) item["class"]);
+        }
+
+        return units; 
+    }
+
 }
